d899338b91e356305f265f940b6cb130d24f026a
galaxyonthehub <117865433+galaxyonthehub@users.noreply.github.com>
2023-07-31 12:08:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(img) {
+        blockImageObject = Img;
+
+        blockImageObject.scaleToWidth(700);
+        blockImageObject.scaleToHeight(510);
+        blockImageObject.set({
+        top:0,
+        left:0
+        });
+        canvas.add(blockImageObject);
+        });
+	
+}
+
+function playSound(){
+	x.play();
+}
